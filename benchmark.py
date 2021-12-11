a38ed884098b2eb1abc6a4d5a5f45749bd0d8d9d
Brent Maas <brent.maas@hotmail.com>
2021-12-11 12:33:16 +0100

    Fix benchmark ordering

--- benchmark.py
+++ benchmark.py
@@ -2,13 +2,14 @@ import os
 import subprocess
 import time
 
-days = [i for i in os.listdir(".") if i.startswith("Day")]
+days = [int(i[3:]) for i in os.listdir(".") if i.startswith("Day")]
+days.sort()
 t0 = time.time()
 for day in days:
     t1 = time.time()
-    subprocess.check_call(["python", "part1.py"], stdout=subprocess.DEVNULL, cwd=day)
-    print(f"{day} part 1: {time.time()-t1:.3f}s")
+    subprocess.check_call(["python", "part1.py"], stdout=subprocess.DEVNULL, cwd=f"Day{day}")
+    print(f"Day{day} part 1: {time.time()-t1:.3f}s")
     t1 = time.time()
-    subprocess.check_call(["python", "part2.py"], stdout=subprocess.DEVNULL, cwd=day)
-    print(f"{day} part 2: {time.time()-t1:.3f}s")
+    subprocess.check_call(["python", "part2.py"], stdout=subprocess.DEVNULL, cwd=f"Day{day}")
+    print(f"Day{day} part 2: {time.time()-t1:.3f}s")
 print(f"Total: {time.time()-t0:.3f}s")
